09069951c676e66fdce45e3919fec7e104d0b097
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-11-21 14:45:45 +0300

    added asteroid class, an asteroid field class, draw and update asteroids. Started using Pygame Groups to render and update game objects like Asteroid and Player to group them together and not worry about calling and updating each individual game object.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -13,6 +16,25 @@ def main():
     current_clock = pygame.time.Clock()
     #delta time for calculating FPS
     dt = 0
+
+    # The Group class is a container that holds and manages multiple game objects. We can organize our objects into various groups to track them more easily.
+    # updatable will hold all the objects that can be updated
+    # drawable will hold all the objects that can be drawn
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
+
+    #instantiate a player object
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     # infinite game loop
     while True:
         log_state()
@@ -22,10 +44,17 @@ def main():
                 return
         # fill screen with a solid black color
         screen.fill("black")
-        #make sure to call this last
+
+        # draw the player
+        updatable.update(dt)
+        asteroids.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+
+        # make sure to call this last
         pygame.display.flip()
-        #the time that passed since last tick() call
-        #convert from milliseconds to seconds
+        # the time that passed since last tick() call
+        # convert from milliseconds to seconds
         dt = current_clock.tick(60)/1000
 
 
